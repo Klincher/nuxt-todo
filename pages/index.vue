7035c91739a91cf49bbd8f4eb0f5b14650146d3a
Klincher <camelotvm48@gmail.com>
2022-09-23 15:46:38 +0300

    shoto sdelal

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,40 @@
 <template>
-  <Tutorial/>
+  <main>
+    <h1>Task Board</h1>
+    <p>Create a list of tasks</p>
+
+    <div class="create-new">
+      <input type="text" v-model="newTask" placeholder="Add a new task" />
+      <button @click="addTask">Add</button>
+    </div>
+
+    <div class="tasks">
+      <Task v-for="(task, i) in $store.state.tasks" :key="i" :task="task" />
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  async mounted() {
+    const tasks = await this.$axios.$get("http://localhost/api/tasks");
+    this.$store.commit("ADD_TASKS", tasks);
+  },
+
+  data() {
+    return {
+      newTask: "",
+      newDescription: "",
+    };
+  },
+
+  methods: {
+    addTask() {
+      if (this.newTask) {
+        this.$store.commit("ADD_TASK", this.newTask);
+        this.newTask = "";
+      }
+    },
+  },
+};
 </script>
---
+++ store/index.js
@@ -0,0 +1,26 @@
+export const state = () => ({
+  tasks: [],
+});
+
+export const mutations = {
+  ADD_TASK(state, task) {
+    this.$axios
+      .$post("http://localhost/api/tasks", {
+        title: task,
+        description: "pizda",
+      })
+      .then((response) => {
+        console.log(response);
+        state.tasks = [{ title: response.title, description: response.description, done: false }, ...state.tasks];
+      });
+  },
+  ADD_TASKS(state, tasks) {
+    state.tasks = [...tasks, ...state.tasks];
+  },
+  REMOVE_TASK(state, task) {
+    state.tasks.splice(state.tasks.indexOf(task), 1);
+  },
+  TOGGLE_TASK(state, task) {
+    task.done = !task.done;
+  },
+};
